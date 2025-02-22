c2707afa0c3177e3c2f10116ee1ceb11c7b021f2
MrOverzealous <overzealouspixels@gmail.com>
2025-02-22 16:17:11 -0500

    Does the core of what I wanted--need to add functionality that interacts with the numbers being cycled; 'max' in particular

--- main.js
+++ main.js
@@ -1,60 +1,71 @@
 // list to cycle through
+const min = 1;
+const max = 'Max';
+
 let quantity = [
     1,
     5,
     10,
     25,
     50,
-    100
+    100,
+    'Max'
 ];
   
-
 function cycle() {
-    const currQty = parseInt(document.querySelector('#cycler').innerText);
-    if (quantity.includes(currQty)) {
-        const nextIndex = quantity.indexOf(currQty) + 1;
-        if (nextIndex === quantity.length) {
-            document.querySelector('#cycler').innerText = quantity[0];
-        }
-        else {
-            document.querySelector('#cycler').innerText = quantity[nextIndex];
-        }
+    let cycleVal = document.querySelector('#cycler').innerText;
+    if (cycleVal === max) {
+        document.querySelector('#cycler').innerText = quantity[0];
+        return;
+    }
+
+    if (quantity.includes(parseInt(cycleVal))) {
+        const nextIndex = quantity.indexOf(parseInt(cycleVal)) + 1;
+        document.querySelector('#cycler').innerText = quantity[nextIndex];
     }
 }
 
 function add() {
     let val = document.querySelector('#customQuantity').value;
-
-    if (parseInt(val) > 0 ) {
-        val = parseInt(val);
-    }
-    else {
-        console.log('bad value, cannot add')
+    // validate
+    if (val === "") { // filter non-numbers
+        console.log('Bad value: Not a number');
         return;
     }
-
+    if (val < 1 || val > 9999) {
+        console.log('Bad value: cannot be less than 1 or greater than 9999');
+        return;
+    }
     if (quantity.includes(val)) {
         console.log("value already present");
         return;
     }
-    else {
-        quantity.push(val);
-        quantity = quantity.toSorted((a, b) => a - b);
 
-        document.querySelector('#quantity').innerHTML = quantity;
-        document.querySelector('#customQuantity').value = "";
-    }
+    quantity.pop();
+    quantity.push(val);
+    quantity = quantity.toSorted((a, b) => a - b);
+    quantity.push(max);
+
+    document.querySelector('#quantity').innerHTML = quantity; // update quantity display
+    document.querySelector('#customQuantity').value = ""; // clear input box
 }
 
 function remove() {
     // removes the value based on the current value of the cycler button, not the input field
-    
-    const valToRemove = parseInt(document.querySelector('#cycler').innerText);
+    const valToRemove = document.querySelector('#cycler').innerText;
+    const valToRemoveInt = parseInt(document.querySelector('#cycler').innerText);
+    // prevent the user from being able to remove the first or last value
+    if (quantity.indexOf(valToRemoveInt) === 0 || quantity.indexOf(valToRemove) === (quantity.length - 1)) {
+        console.log('Cannot remove 1 or Max')
+        return;
+    }
     const qty = quantity.filter((val) => val !== valToRemove);
-    cycle()
+    cycle();
     quantity = qty;
     document.querySelector('#quantity').innerHTML = quantity;
 }
 
 document.querySelector('#quantity').innerText = quantity;
-// document.querySelector('#customQuantity').addEventListener('')
+
+
+
