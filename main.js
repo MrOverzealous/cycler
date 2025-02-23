16edb77973ef2cbef7448dd7bf7bbc3fb3742275
MrOverzealous <overzealouspixels@gmail.com>
2025-02-23 00:21:22 -0500

    Does what I wanted it to do. Might reorganize and restyle the elements but it's  functionally there. Add/Remove could be made more clear, but I'd probably do that, at least partially, by reorganizing.

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
 // list to cycle through
 const min = 1;
 const max = 'Max';
+let currency = 5000;
+
+const mines = {
+    name: "Mines",
+    count: 0,
+    cost: 50
+}
 
 let quantity = [
     1,
@@ -26,46 +33,73 @@ function cycle() {
 }
 
 function add() {
-    let val = document.querySelector('#customQuantity').value;
+    let valToAdd = document.querySelector('#customQuantity').value;
     // validate
-    if (val === "") { // filter non-numbers
+    if (valToAdd === "") { // filter non-numbers
         console.log('Bad value: Not a number');
         return;
     }
-    if (val < 1 || val > 9999) {
+    if (valToAdd < 1 || valToAdd > 9999) {
         console.log('Bad value: cannot be less than 1 or greater than 9999');
         return;
     }
-    if (quantity.includes(val)) {
+    if (quantity.includes(valToAdd)) {
         console.log("value already present");
         return;
     }
 
     quantity.pop();
-    quantity.push(val);
+    quantity.push(valToAdd);
     quantity = quantity.toSorted((a, b) => a - b);
     quantity.push(max);
 
-    document.querySelector('#quantity').innerHTML = quantity; // update quantity display
     document.querySelector('#customQuantity').value = ""; // clear input box
+
+    updateScreen();
 }
 
 function remove() {
     // removes the value based on the current value of the cycler button, not the input field
     const valToRemove = document.querySelector('#cycler').innerText;
     const valToRemoveInt = parseInt(document.querySelector('#cycler').innerText);
+
     // prevent the user from being able to remove the first or last value
     if (quantity.indexOf(valToRemoveInt) === 0 || quantity.indexOf(valToRemove) === (quantity.length - 1)) {
-        console.log('Cannot remove 1 or Max')
+        console.log('Cannot remove 1 or Max');
         return;
     }
     const qty = quantity.filter((val) => val !== valToRemove);
     cycle();
     quantity = qty;
-    document.querySelector('#quantity').innerHTML = quantity;
+
+    updateScreen();
 }
 
-document.querySelector('#quantity').innerText = quantity;
+
+function buy(item) {
+    const buyQty = document.querySelector('#cycler').innerText;
+    const totalCost = parseInt(buyQty) * item.cost;
+    
+    if (buyQty === max) {
+        const qtyAffordable = Math.floor(currency / item.cost);
+        item.count += qtyAffordable;
+        currency -= qtyAffordable * item.cost;
+    }
+    else if (totalCost < currency) {
+        item.count += parseInt(buyQty);
+        currency -= totalCost;
+    }
+    else {
+        // hand not enough currency, if necessary
+    }
+    updateScreen();
+}
 
 
+function updateScreen() {
+    document.querySelector('#currency').innerText = `Currency: ${currency}`;
+    document.querySelector('#quantity').innerText = quantity;
+    document.querySelector('#mines').innerText = `${mines.name} Count: ${mines.count}`;
+}
 
+updateScreen();
